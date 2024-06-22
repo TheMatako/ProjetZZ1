f96e9b170a5582bf84e8a30217b2e64adbe14b7b
ForoyV8 <verin.clement03@gmail.com>
2024-06-22 14:35:36 +0200

    Generer_Coup fonctionne (Manque les barrières)

--- Quoridor/Min_Max.c
+++ Quoridor/Min_Max.c
@@ -3,45 +3,7 @@
 # include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-
-// Pour les Cases
-#define BOX_WIDTH 48
-#define BOX_HEIGHT 48
-#define BOX_NUMBER_LINE 9
-#define BOX_NUMBER_COLUMN 9
-#define TOTAL_BOX_NUMBER BOX_NUMBER_LINE * BOX_NUMBER_COLUMN 
-
-typedef struct {
-    int x;  // Coordonnée x
-    int y;  // Coordonnée y
-} Position;
-
-typedef struct {
-    Position pos;    // Position de la barrière
-    int isHorizontal; // 1 pour horizontal, 0 pour vertical
-} Barrier;
-
-typedef struct {
-    Position pos;      // Position actuelle du pion
-    int barriersLeft;  // Nombre de barrières restantes
-    SDL_Texture *sprite;
-} Player;
-
-typedef struct {
-    Position pos; // Position de la case
-    Position posPixel; // Position de la case en pixels
-} Box;
-
-typedef struct {
-    Player * players; // Joueurs (0: humain, 1: IA)
-    Barrier * barriers; // Toutes les barrières placées sur le plateau
-    Position matrixBarriersPosition[8][8];
-    Box boxes[BOX_NUMBER_LINE][BOX_NUMBER_COLUMN]; // Toutes les (positions des) cases sur le plateau
-    int barriersCount;     // Nombre actuel de barrières placées
-    bool isGameRunning;
-    int playerTurn;
-    
-} GameState;
+#include "structureQuoridor.h"
 
 typedef struct Coup
 {   
@@ -77,13 +39,23 @@ Liste_Coups_t * Creer_Liste_Coups()
 Liste_Coups_t * Ajouter_Coup_Liste(Liste_Coups_t * L, int xp, int yp ,int xb ,int yb ,int H)
 {
     Coup_t * NewC = malloc(sizeof(Coup_t));
+    Position * NewP = malloc(sizeof(Position));
+    Barrier * NewB = malloc(sizeof(Barrier));
+    Position * NewPB = malloc(sizeof(Position));
 
-    if(NewC)
+    if(NewC && NewP && NewB && NewPB)
     {
+        NewP->x = xp; NewP->y = yp;
 
-        NewC->NewPos->x = xp; NewC->NewPos->y = yp;
-        NewC->NewBar->pos.x = xb; NewC->NewBar->pos.y = yb;
-        NewC->NewBar->isHorizontal = H;
+        NewC->NewPos = NewP;
+
+        NewPB->x = xb; NewPB->x = yb;
+
+        NewB->pos = *NewPB;
+        NewB->isHorizontal = H;
+
+        NewC->NewPos = NewP;
+        NewC->NewBar = NewB;
 
         // Cas 1: La liste est vide
         if (L->Tete == NULL) 
@@ -107,9 +79,13 @@ void Affichage (Coup_t * Tete)
     Coup_t * Courant = Tete;
     Coup_t * Dernier = NULL;
 
-    while (Courant != NULL) 
+    printf("XP YP XB YB Sens \n\n");
+
+    while (Courant != NULL)
     {
-        printf("| %p %d %d %p | -> ", Courant->Prec,Courant->NewPos->x,Courant->NewPos->y,Courant->Suiv);
+        printf("| %d %d %d %d %d | -> "/* Courant->Prec*/,Courant->NewPos->x,Courant->NewPos->y,
+                                    Courant->NewBar->pos.x,Courant->NewBar->pos.y,
+                                    Courant->NewBar->isHorizontal/*Courant->Suiv*/);
 
         // Mise à jour du dernier noeud visité
         Dernier = Courant;
@@ -135,6 +111,7 @@ void Initialiser_Liste(int T[8][8])
         for(j=0;j<=7;j++)
         {
             T[i][j] = 0;
+            printf("%d",T[i][j]);
         }
     }
 }
@@ -149,7 +126,7 @@ bool Present(int T[8][8],int x,int y)
 
 int Compare_Place(Position * Pos1,Position * Pos2)
 {
-    if(Pos1->x == Pos2->x && Pos1->y== Pos2->y)
+    if(Pos1->x == Pos2->x && Pos1->y == Pos2->y)
         return 1;
     return 0;
 }
@@ -172,10 +149,11 @@ int Is_Diagonal_or_Simple_Moove(Position * Previous, Position * Next)
 }
 
 bool Is_There_An_Obstacle(GameState * jeu, Position * Previous, Position * Next)
-{ // True : Il y a un obstacle ,False : Aucun Obstacle
+{ // True : Il y a un obstacle , False : Aucun Obstacle
     int i; bool O = false;
 
-    if((Next->y <= BOX_NUMBER_LINE) && (Next->x <= BOX_NUMBER_COLUMN) && (!Compare_Place(&jeu->players[0].pos,&jeu->players[1].pos)))
+    if((Next->y >= 0) && ((Next->x >= 0)) && (Next->y < BOX_NUMBER_LINE) && (Next->x < BOX_NUMBER_COLUMN) 
+    && !Compare_Place(Next,&jeu->players[0].pos) && !Compare_Place(Next,&jeu->players[1].pos))
     {
         int Direction = Is_Diagonal_or_Simple_Moove(Previous,Next);
 
@@ -187,9 +165,9 @@ bool Is_There_An_Obstacle(GameState * jeu, Position * Previous, Position * Next)
                 {   
                     if(jeu->barriers[i].isHorizontal == 1)
                     {
-                        if(Previous->y + 1 == Next->y && (jeu->barriers[i].pos.x == Previous->x || jeu->barriers[i].pos.x == Previous->x-1) && jeu->barriers[i].pos.y == Next->y)
+                        if(Previous->y + 1 == Next->y && (jeu->barriers[i].pos.x == Next->x || jeu->barriers[i].pos.x == Next->x - 1) && jeu->barriers[i].pos.y == Next->y)
                             O = true;
-                        else if(Previous->y - 1 == Next->y && (jeu->barriers[i].pos.x == Previous->x || jeu->barriers[i].pos.x == Previous->x-1) && jeu->barriers[i].pos.y == Previous->y)
+                        else if(Previous->y - 1 == Next->y && (jeu->barriers[i].pos.x == Next->x || jeu->barriers[i].pos.x == Next->x - 1) && jeu->barriers[i].pos.y == Previous->y)
                             O = true;
                     }
                 }
@@ -201,15 +179,15 @@ bool Is_There_An_Obstacle(GameState * jeu, Position * Previous, Position * Next)
                 {
                     if(jeu->barriers[i].isHorizontal == 0)
                     {
-                        if(Previous->x + 1 == Next->x && jeu->barriers[i].pos.x == Previous->x && (jeu->barriers[i].pos.y == Previous->y || jeu->barriers[i].pos.y == Previous->y+1))
+                        if(Previous->x + 1 == Next->x && (jeu->barriers[i].pos.y == Previous->y || jeu->barriers[i].pos.y == Previous->y + 1) && jeu->barriers[i].pos.x == Previous->x)
                             O = true;
-                        else if(Previous->x - 1 == Next->x && jeu->barriers[i].pos.x == Next->x && (jeu->barriers[i].pos.y == Previous->y || jeu->barriers[i].pos.y == Previous->y+1))
+                        else if(Previous->x - 1 == Next->x && (jeu->barriers[i].pos.y == Next->y || jeu->barriers[i].pos.y == Next->y + 1) && jeu->barriers[i].pos.x == Next->x)
                             O = true;
                     }
                 }
                 break;
 
-            case -1 :
+            /* case -1 :
 
                 if(Previous->y + 1 == Next->y && Previous->x + 1 == Next->x)
                 {
@@ -275,7 +253,10 @@ bool Is_There_An_Obstacle(GameState * jeu, Position * Previous, Position * Next)
                         }
                     }
                 }
-                break;
+                break;*/
+
+                default :
+                    break;
         }
     }
     else
@@ -297,28 +278,28 @@ Liste_Coups_t * Generer_Coup(GameState * jeu, int Joueur)
 
     Nouvelle_Position.y = Nouvelle_Position.y + 1;
 
-    if(Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller en avant */
-        L = Ajouter_Coup_Liste(L,Place.x,Place.y+1,0,0,0);
+    if(!Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller en avant */
+        L = Ajouter_Coup_Liste(L,Place.x,Place.y + 1,0,0,0);
     
     Nouvelle_Position.y = Nouvelle_Position.y - 2;
 
-    if (Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller en arrière*/
-        L = Ajouter_Coup_Liste(L,Place.x,Place.y-1,0,0,0);
+    if (!Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller en arrière*/
+        L = Ajouter_Coup_Liste(L,Place.x,Place.y - 1,0,0,0);
 
-    Nouvelle_Position.y= Nouvelle_Position.y+ 1;
+    Nouvelle_Position.y = Nouvelle_Position.y + 1;
     Nouvelle_Position.x = Nouvelle_Position.x - 1;
     
-    if (Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller à Gauche */
-        L = Ajouter_Coup_Liste(L,Place.x-1,Place.y,0,0,0);
+    if (!Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller à Gauche */
+        L = Ajouter_Coup_Liste(L,Place.x - 1,Place.y,0,0,0);
     
     Nouvelle_Position.x = Nouvelle_Position.x + 2;
 
-    if (Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller à droite*/
-        L = Ajouter_Coup_Liste(L,Place.x+1,Place.y,0,0,0);
+    if (!Is_There_An_Obstacle(jeu,&Place,&Nouvelle_Position)) /* Si il est possible d'aller à droite*/
+        L = Ajouter_Coup_Liste(L,Place.x + 1,Place.y,0,0,0);
         
     // On préfère poser une barrière
 
-    if(jeu->players[Joueur].barriersLeft)
+    /* if(jeu->players[Joueur].barriersLeft)
     {    
         int Occupees[8][8];
         Initialiser_Liste(Occupees);
@@ -347,29 +328,83 @@ Liste_Coups_t * Generer_Coup(GameState * jeu, int Joueur)
                 }
             }
         }    
-    }
+    }*/
 
     return L;
 }
 
+GameState initGame()
+{
+    GameState jeu = 
+    {
+        .players = {{.pos = {4, 8}, .barriersLeft = 10}, {.pos = {4, 0}, .barriersLeft = 10}},
+        .barrierCount = 0,
+        .isGameRunning = true,
+        .playerTurn = 0
+    };
+
+    for (int i = 0; i < BOX_NUMBER_LINE; i++) 
+    {
+        for (int j = 0; j < BOX_NUMBER_COLUMN; j++)
+        {
+            // On initialise dans le tableau jeu.boxes les positions en pixel des cases
+            jeu.boxes[i][j].posPixel.x = SPACE_LENGTH + i*(BOX_WIDTH+SPACE_LENGTH);
+            jeu.boxes[i][j].posPixel.y = TOP_LENGTH + SPACE_LENGTH + j*(BOX_HEIGHT+SPACE_LENGTH);
+        }
+    }
+
+    return jeu;
+}
+
 int main()
 {
-    GameState * jeu = malloc(sizeof(GameState));
-    Player * J0 = malloc(sizeof(Player));
+    GameState jeu = initGame();
+    /* Player * J0 = malloc(sizeof(Player));
     Player * J1 = malloc(sizeof(Player));
     Position * P0 = malloc(sizeof(Position));
-    Position * P1 = malloc(sizeof(Position));
+    Position * P1 = malloc(sizeof(Position)); */
 
-    P0->x = 4; P0->y = 9;
+    /* P0->x = 4; P0->y = 9;
     P1->x = 4; P1->y = 0;
 
     J0->pos = *P0; J1->pos = *P1;
     J0->barriersLeft = 10; J1->barriersLeft = 10;
 
     jeu->players[0] = *J0;
-    jeu->players[1] = *J1;
+    jeu->players[1] = *J1; */
 
-    Liste_Coups_t * L = Generer_Coup(jeu,1);
+    /* Liste_Coups_t * L = Creer_Liste_Coups();
+    L = Ajouter_Coup_Liste(L,4,8,0,0,0);
+    L = Ajouter_Coup_Liste(L,5,8,0,0,0);
+    L = Ajouter_Coup_Liste(L,5,7,0,0,0);
+    L = Ajouter_Coup_Liste(L,4,8,1,2,1); */
+
+    /* int T[8][8];
+
+    Initialiser_Liste(T);
+    Add(T,4,7);
+    printf("%d %d",T[4][7],T[7][4]);*/
+
+    Position * P0 = malloc(sizeof(Position));
+    Position * P1 = malloc(sizeof(Position)); 
+
+    P0->x = jeu.players[0].pos.x; 
+    P0->y = jeu.players[0].pos.y;
+
+    P0->x = 4;
+    P0->y = 8;
+    P1->x = P0->x;
+    P1->y = P0->y;
+
+    jeu.barriers[0].pos.x = P0->x-1;
+    jeu.barriers[0].pos.y = P0->y;
+    jeu.barriers[0].isHorizontal = 0;
+
+    // printf("%d",Is_Diagonal_or_Simple_Moove(P0,P1));
+
+    // printf("%d",Is_There_An_Obstacle(&jeu,P0,P1));
+
+    Liste_Coups_t * L = Generer_Coup(&jeu,0);
 
     Affichage(L->Tete);
 }
